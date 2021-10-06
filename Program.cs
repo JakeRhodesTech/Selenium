f2d6160dbc82b8f0adaa11f8bb3901ab33e36bf0
JakeRhodesTech <83876985+JakeRhodesTech@users.noreply.github.com>
2021-10-06 14:38:53 +0100

    Add files via upload
    Writing Test Method using NUnit, Selenium. Execute Automation (4/22)

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace TestMethodNUnit
+{
+    class Program
+    {
+
+        //Create the reference for our browser
+        IWebDriver driver = new ChromeDriver();
+        
+        static void Main(string[] args)
+        {
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            //Navigate to Google page 
+            driver.Navigate().GoToUrl("http://www.google.com");
+            Console.WriteLine("Opened the browser");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            //Find the element
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            //Perform Ops 
+            element.SendKeys("executeautomation");
+            Console.WriteLine("Performed operations");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+            Console.WriteLine("Closed the browser");
+        }
+    }
+}
