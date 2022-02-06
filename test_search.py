5b6f2ed50473dfcd605bbcec88d023f906ae9c86
JakeRhodesTech <83876985+JakeRhodesTech@users.noreply.github.com>
2022-02-06 15:36:26 +0000

    Add files via upload
    Example test to run with Pytest.
    Automation Panda, Selenium WebDriver with Python, Chapter 2

---
+++ test_search.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+def test_basic_duckduckgo_search(): 
+    
+    #Given the DuckDuckGo home page is displayed
+    # TODO
+    
+    #when the user searches for "panda"
+    #TODO
+    
+    #The the search result title contains "panda"
+    #TODO
+    
+    #And the search result query is "panda"
+    #TODO
+    
+    #And the search result links pertain to "panda"
+    #TODO
+    
+    raise Exception("Incomplete Test")
+
+
+# In[ ]:
+
+
+
+
